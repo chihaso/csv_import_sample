8e12ab64c5a11f1011f50c878af5047ae609484b
chihaso <cycyccq@gmail.com>
2022-04-02 19:29:30 +0900

    parse処理の中の処理をメソッドに切り出して整理する

--- app/models/population/csv/importer/parser.rb
+++ app/models/population/csv/importer/parser.rb
@@ -17,18 +17,9 @@ class Population::Csv::Importer::Parser
         current_prefecture = row['都道府県']
         nil
       elsif row['市区町村'].present?
-        municipality_info = registered_municipalities_info.find do |info|
-          info[1] == current_region && info[2] == current_prefecture && info[3] == row['市区町村']
-        end
-
-        if municipality_info.present?
-          years.map do |year|
-            {
-              municipality_id: municipality_info[0],
-              year: year,
-              value: row["#{year}年"].to_i
-            }
-          end
+        if municipality_info(current_region, current_prefecture, row).present?
+          municipality_id = municipality_info(current_region, current_prefecture, row)[0]
+          years.map { |year| population_data(municipality_id, year, row) }
         else
           raise '登録されていない市区町村のデータはアップロードできません'
         end
@@ -40,6 +31,12 @@ class Population::Csv::Importer::Parser
 
   private
 
+  def municipality_info(current_region, current_prefecture, row)
+    registered_municipalities_info.find do |info|
+      info[1] == current_region && info[2] == current_prefecture && info[3] == row['市区町村']
+    end
+  end
+
   def registered_municipalities_info
     # 各行で市区町村が登録済みか調べようとするとN+1問題が発生するので、配列にまとめておく
     columns = %w[municipalities.id regions.name prefectures.name municipalities.name]
@@ -48,5 +45,13 @@ class Population::Csv::Importer::Parser
 
   def years
     @years ||= @csv.headers.drop(3).map { |year_str| year_str.chop.to_i }
+  end
+
+  def population_data(municipality_id, year, row)
+    {
+      municipality_id: municipality_id,
+      year: year,
+      value: row["#{year}年"].to_i
+    }
   end
 end
