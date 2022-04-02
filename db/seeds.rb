78c27150579ee09ea796f781a30138595f7e5298
chihaso <cycyccq@gmail.com>
2022-04-02 18:17:24 +0900

    シードデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Region
+tohoku = Region.create!(name: '東北地方')
+kanto = Region.create!(name: '関東地方')
+
+# Prefecture
+iwate = tohoku.prefectures.create!(name: '岩手県')
+akita = tohoku.prefectures.create!(name: '秋田県')
+tokyo = kanto.prefectures.create!(name: '東京都')
+kanagawa = kanto.prefectures.create!(name: '神奈川県')
+
+# Municipality
+iwate.municipalities.create!(name: '盛岡市')
+iwate.municipalities.create!(name: '遠野市')
+akita.municipalities.create!(name: '秋田市')
+akita.municipalities.create!(name: '男鹿市')
+tokyo.municipalities.create!(name: '新宿区')
+tokyo.municipalities.create!(name: '奥多摩町')
+kanagawa.municipalities.create!(name: '横浜市')
+kanagawa.municipalities.create!(name: '清川村')
