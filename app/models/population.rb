19ef6026e4eb6ac1868a99d62d3271a1732edf5b
chihaso <cycyccq@gmail.com>
2022-04-12 11:01:52 +0900

    populationのバリデーション追加

--- app/models/population.rb
+++ app/models/population.rb
@@ -1,3 +1,7 @@
 class Population < ApplicationRecord
   belongs_to :municipality
+
+  validates :year, :value, presence: true
+  validates :year, numericality: { only_integer: true }, if: -> { year.present? }
+  validates :value, numericality: { only_integer: true }, if: -> { value.present? }
 end
